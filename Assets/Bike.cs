51105182c99786e2cddff8c21864a3a01a84012d
SAkana <d.anabuki-college.net>
2022-10-22 20:03:57 +0900

    自転車

--- Assets/Bike.cs
+++ Assets/Bike.cs
@@ -2,34 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bike : MonoBehaviour
-{/// <summary>
- ///ïœêî0.05ÇÅAfloatï˚Ç≈çÏê¨ÇµÇΩmoveÇ…ì¸ÇÍÇÈ
- /// </summary>
-    float move = 0.05f;
-    public GameObject[] prefabs
-        = new GameObject[4];
-    // Start is called before the first frame update
-    /*void Start()
+public class Bike : MonoBehaviour, SampleInterface
+{
+    public float FloatFunc()
     {
-        
-    }*/
+        return 100;
+    }
+
+    public int IntFunc()
+    {
+        return -1;
+    }
+
+    public Vector2 VectorFunc()
+    {
+        return Vector2.zero;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.left;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = transform.position;
-        pos.x += move;
-        if (pos.x < -2.5f)
-        {
-            move = 0.05f;
-        }
-        if (pos.x > 2.5f)
-        {
-            move = -0.05f;
-        }
-        transform.position = pos;
-
 
     }
 }
