82b99db5282c4dd5d320a94af8e47faf34814979
gongitsune <89068705+gongitsune@users.noreply.github.com>
2022-10-22 20:12:20 +0900

    タイルマップ追加

---
+++ Assets/keigo/Scripts/Common/IObstacle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace keigo.Scripts.Common
+{
+    /// <summary>
+    /// 障害物のインターフェース
+    /// </summary>
+    public interface IMovingObstacle
+    {
+        
+    }
+
+    /// <summary>
+    /// スピードアップ等の副作用がある障害物
+    /// </summary>
+    public interface IEffectObstacle
+    {
+        /// <summary>
+        /// これに当たった時のスピードの変化量
+        /// </summary>
+        float SpeedChangeAmount { get; }
+    }
+}
