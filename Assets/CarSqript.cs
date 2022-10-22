615d01ff0c012c75af0986173fc5c35ed0c94399
chogeru <108376848+chogeru@users.noreply.github.com>
2022-10-22 19:44:38 +0900

    スクリプト追加

---
+++ Assets/CarSqript.cs
@@ -0,0 +1,40 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class CarSqript : MonoBehaviour,  SqriptInterface
+{
+    public float FloartFunc()
+    {
+        return 100;
+    }
+    public int IntFunc()
+    {
+        return -1;
+    }
+
+    public Vector2 VectorFunc()
+    {
+        return Vector2.zero;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+internal interface SqriptInterface
+{
+}
