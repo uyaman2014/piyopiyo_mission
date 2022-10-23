cbe040673d7bd1cfd1fad5c39e00c4f0babd12f8
chogeru <108376848+chogeru@users.noreply.github.com>
2022-10-23 14:27:02 +0900

    a

---
+++ Assets/hayashi/kusa.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kusa : MonoBehaviour
+{
+    int moovspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision2D col)
+    {
+       
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
