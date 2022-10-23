583dfb6ca875615094d4caf75d32ca0f0eebfa8b
keigo <89068705+gongitsune@users.noreply.github.com>
2022-10-23 11:29:31 +0900

    GameStateManager追加

--- Assets/keigo/Scripts/Manager/GameStateManager.cs
+++ Assets/keigo/Scripts/Manager/GameStateManager.cs
@@ -1,13 +1,16 @@
-﻿using keigo.Scripts.Common;
+﻿using System;
+using UniRx;
 using UnityEngine;
 
 namespace Manager
 {
     /// <summary>
-    /// ゲーム全体のステート管理
+    ///     ゲーム全体のステート管理
     /// </summary>
     public class GameStateManager : MonoBehaviour
     {
+        private ReactiveProperty<GameState> _currentState;
+
         public static GameStateManager Instance
         {
             get
@@ -17,26 +20,41 @@ namespace Manager
             }
         }
 
-        private GameState _currentState;
+        public IObservable<GameState> OnStateChange => _currentState;
 
+        /// <summary>
+        ///     ステートを変更
+        /// </summary>
+        /// <param name="newState">新しいステート</param>
         public void PublishState(GameState newState)
         {
-            
+            _currentState.Value = newState;
+        }
+
+        /// <summary>
+        ///     現在のステートを取得
+        /// </summary>
+        /// <returns></returns>
+        public GameState GetCurrentState()
+        {
+            return _currentState.Value;
         }
     }
 
     public enum GameState
     {
         /// <summary>
-        /// タイトル画面
+        ///     タイトル画面
         /// </summary>
         Title,
+
         /// <summary>
-        /// プレイ中
+        ///     プレイ中
         /// </summary>
         Playing,
+
         /// <summary>
-        /// ポーズ中
+        ///     ポーズ中
         /// </summary>
         Pause
     }
