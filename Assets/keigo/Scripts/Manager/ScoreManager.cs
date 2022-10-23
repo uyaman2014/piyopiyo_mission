23c53d8d166bde075475446219cb7121b3bbf8a4
keigo <89068705+gongitsune@users.noreply.github.com>
2022-10-23 10:09:18 +0900

    ScoreManager追加

---
+++ Assets/keigo/Scripts/Manager/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Manager
+{
+    public class ScoreManager: MonoBehaviour
+    {
+        public static ScoreManager Instance
+        {
+            get
+            {
+                GameObject.FindWithTag("ScoreManager").TryGetComponent(out ScoreManager manager);
+                return manager;
+            }
+        }
+
+        private int _currentScore;
+
+        /// <summary>
+        /// スコアを増やす
+        /// </summary>
+        /// <param name="amount">増やす量(マイナスだったら減ります)</param>
+        public void IncreaseScore(int amount)
+        {
+            _currentScore += amount;
+        }
+    }
+}
