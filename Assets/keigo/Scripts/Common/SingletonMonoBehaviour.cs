cef1ae046a44967c73c1c83124b078bbc680af6e
gongitsune <89068705+gongitsune@users.noreply.github.com>
2022-10-29 13:35:06 +0900

    SingltonMonoBehaviour追加

---
+++ Assets/keigo/Scripts/Common/SingletonMonoBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace keigo.Scripts.Common
+{
+    public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T>
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                _instance ??= new GameObject().AddComponent<T>();
+                return _instance;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _instance = null;
+        }
+    }
+}
---
+++ Assets/keigo/Scripts/Manager/SEManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Manager
+{
+    /// <summary>
+    /// 効果音マネージャー
+    /// </summary>
+    public class SeManager : MonoBehaviour
+    {
+         
+    }
+}
